974ca535a466ecc09f0ee5d4375dc8b9a66d4de6
Shibam Sadhukhan <sadhukhan799@gmail.com>
2026-02-05 23:11:24 +0530

    .

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-   // for (int i = n - 1; i >= 0; i--)
-    // {
-    //     if (height[i] == maxRight[n - 2])
-    //     {
-    //         rightIdx = i;
-    //         break;
-    //     }
-    // }
